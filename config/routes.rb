64c997abdfd80be4d1648dc376bb10660b74f9ff
Hendra Gunawan <hendragunz@codecampz.com>
2015-04-11 00:04:10 +0700

    improve redirection, disable sesions controller, just use facebook to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Prelaunchr::Application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }, skip: [:sessions]
+
   root :to => "dashboard#show"
 
-  get 'refer-a-friend' => 'dashboard#refer', as: :user_refer
+  get 'refer-a-friend' => 'dashboard#refer',  as: :user_refer
   get 'privacy-policy' => 'dashboard#policy', as: :user_policy
+  get 'top-list'       => 'dashboard#top',    as: :user_top
 
   unless Rails.application.config.consider_all_requests_local
-      match '*not_found', to: 'dashboard#redirect', :format => false
+    match '*not_found', to: 'dashboard#redirect', :format => false
   end
 end
